82a5d294513012ac3f6e3325e76d766e353edbc8
EmilioGomez3528 <emitrooper@outlook.com>
2024-09-11 23:59:42 -0600

    Carpeta shared creada

--- angular1.client/src/app/app.module.ts
+++ angular1.client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
